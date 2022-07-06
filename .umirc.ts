7ec8011f2d6e41b3c3abdf9668e18fab081a0f9d
louyl <louyl@wedoctor.com>
2022-07-06 18:15:25 +0800

    feat: 补充 title、meta headers

--- .umirc.ts
+++ .umirc.ts
@@ -6,4 +6,12 @@ export default {
     { path: '/', component: 'index' },
     { path: '/about', component: 'About' },
   ],
+  title: 'Grid - Web Designer',
+  metas: [
+    { name: 'keywords', content: 'grid,designer,builder,design,build' },
+    {
+      name: 'description',
+      content: 'A professional website designer for you project',
+    },
+  ],
 };
