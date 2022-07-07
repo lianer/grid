c5362e1584bdbc5ee29ada15fe5239d7ada9fe94
louyl <louyl@wedoctor.com>
2022-07-07 23:00:13 +0800

    feat: 兼容 BasicControl autoWidth

--- src/types/types-control.ts
+++ src/types/types-control.ts
@@ -4,7 +4,7 @@ export type ControlSchema = BasicControlSchema | AutoHeightControlSchema;
 // 标准操作组件
 export type BasicControlSchema = {
   type: 'BasicControlSchema';
-  width: number;
+  width?: number;
   height?: number;
   left: number;
   top: number;
