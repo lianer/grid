97ec1748b4b7be7be40fe7fe9b0d2dacec97545a
louyl <louyl@wedoctor.com>
2022-07-11 19:14:05 +0800

    feat: 优化 Image 异常逻辑

--- src/_materials/image/Component.tsx
+++ src/_materials/image/Component.tsx
@@ -1,4 +1,5 @@
-import { FC } from 'react';
+import { isEmpty } from 'lodash-es';
+import { FC, useEffect, useState } from 'react';
 import { ImageSchema } from './schema';
 
 const Component: FC<ImageSchema> = ({
@@ -6,20 +7,24 @@ const Component: FC<ImageSchema> = ({
   onCompUpdate,
   attrs: { src, opacity },
 }) => {
-  return (
+  const [fallback, setFallback] = useState(isEmpty(src));
+
+  useEffect(() => {
+    setFallback(false);
+  }, [src]);
+
+  return fallback ? (
+    <div className="FallbackImage w-full h-[6rem] bg-gray-200 text-center leading-[6rem] text-[1.125rem] text-gray-400">
+      默认图
+    </div>
+  ) : (
     <img
       src={src.value}
       style={{
         width: '100%',
         opacity: opacity.value,
       }}
-      onError={(e) => {
-        if (stage && onCompUpdate) {
-          onCompUpdate({
-            src: { value: 'https://kano.guahao.com/1QE687641013' },
-          });
-        }
-      }}
+      onError={(e) => setFallback(true)}
     />
   );
 };
